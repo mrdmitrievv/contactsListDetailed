16e9d1098c10b2bc00ea7384cce5081636b58a56
mrdmitrievv <mrdmitrievv@yandex.ru>
2022-03-28 16:12:12 +0300

    Added new feature: adding new contact

---
+++ contactsListDetailed/View Controllers/NewContactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewContactViewController.swift
+//  contactsListDetailed
+//
+//  Created by Артём Дмитриев on 26.03.2022.
+//
+
+import UIKit
+
+class NewContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
